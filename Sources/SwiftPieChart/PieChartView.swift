c61bf69a921245ec8edcc2291503af9420024900
marcello <m.morellato@ferlini.it>
2022-05-24 18:51:30 +0200

    chore: add color configuration property and minor layout changes

--- Sources/SwiftPieChart/PieChartView.swift
+++ Sources/SwiftPieChart/PieChartView.swift
@@ -19,6 +19,8 @@ public struct PieChartView: View {
     
     public var widthFraction: CGFloat
     public var innerRadiusFraction: CGFloat
+    public var accentColor: Color
+    public var accentLightColor: Color
     
     @State private var activeIndex: Int = -1
     
@@ -35,7 +37,10 @@ public struct PieChartView: View {
         return tempSlices
     }
     
-    public init(values:[Double], names: [String], formatter: @escaping (Double) -> String, colors: [Color] = [Color.blue, Color.green, Color.orange], backgroundColor: Color = Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0), widthFraction: CGFloat = 0.75, innerRadiusFraction: CGFloat = 0.60, foregroundColor: Color = .black){
+    public init(values:[Double], names: [String], formatter: @escaping (Double) -> String, colors: [Color] = [Color.blue, Color.green, Color.orange],
+                backgroundColor: Color = Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0),
+                widthFraction: CGFloat = 0.75, innerRadiusFraction: CGFloat = 0.60,
+                foregroundColor: Color = .black, accentColor: Color = Color.green, accentLightColor: Color = Color.green.opacity(0.6)) {
         self.values = values
         self.names = names
         self.formatter = formatter
@@ -45,13 +50,15 @@ public struct PieChartView: View {
         self.widthFraction = widthFraction
         self.innerRadiusFraction = innerRadiusFraction
         self.foregroundColor = foregroundColor
+        self.accentLightColor = accentLightColor
+        self.accentColor = accentColor
     }
     
     public var body: some View {
         GeometryReader { geometry in
-            HStack{
-                ZStack{
-                    ForEach(0..<self.values.count){ i in
+            HStack {
+                ZStack {
+                    ForEach(0..<self.values.count) { i in
                         PieSlice(pieSliceData: self.slices[i])
                             .scaleEffect(self.activeIndex == i ? 1.03 : 1)
                             .animation(Animation.spring())
@@ -87,16 +94,24 @@ public struct PieChartView: View {
                         .fill(self.backgroundColor)
                         .frame(width: widthFraction * geometry.size.width * innerRadiusFraction, height: widthFraction * geometry.size.width * innerRadiusFraction)
                     
-                    VStack {
-                        Text(self.activeIndex == -1 ? "Total" : names[self.activeIndex])
-                            .font(.title)
+                    VStack(spacing: 4) {
+                        Text("Total".uppercased())
+                            .font(.system(size: 13))
                             .foregroundColor(Color.gray)
                         Text(self.formatter(self.activeIndex == -1 ? values.reduce(0, +) : values[self.activeIndex]))
-                            .font(.title)
+                            .font(.system(size: 28)).bold().foregroundColor(accentColor)
+                        Text(self.activeIndex == -1 ? "" : names[self.activeIndex].uppercased())
+                            .font(.system(size: 13))
+                            .foregroundColor(Color.gray)
                     }
                     
                 }
-                PieChartRows(colors: self.colors, names: self.names, values: self.values.map { self.formatter($0) }, percents: self.values.map { String(format: "%.0f%%", $0 * 100 / self.values.reduce(0, +)) }).padding(20)
+                HStack {
+                    Spacer()
+                PieChartRows(colors: self.colors, names: self.names, values: self.values.map { self.formatter($0) },
+                             percents: self.values.map { String(format: "%.0f%%", $0 * 100 / self.values.reduce(0, +)) }, accentColor: accentColor, accentLightColor: accentLightColor).padding(20)
+                    Spacer()
+                }
             }
             .background(self.backgroundColor)
             .foregroundColor(self.foregroundColor)
@@ -110,33 +125,43 @@ struct PieChartRows: View {
     var names: [String]
     var values: [String]
     var percents: [String]
+    var accentColor: Color
+    var accentLightColor: Color
     
     var body: some View {
-        VStack{
-            ForEach(0..<self.values.count){ i in
-                HStack {
-                    RoundedRectangle(cornerRadius: 5.0)
+        VStack(alignment: .leading) {
+            ForEach(0..<self.values.count) { i in
+                HStack(alignment: .center, spacing: 30) {
+                    Circle()
                         .fill(self.colors[i])
                         .frame(width: 20, height: 20)
+                    
+                    Text(self.percents[i]).font(.system(size: 24))
+                        .foregroundColor(accentColor).bold()
+                    
                     Text(self.names[i])
                     Spacer()
-                    VStack(alignment: .trailing) {
-                        Text(self.values[i])
-                        Text(self.percents[i])
-                            .foregroundColor(Color.gray)
+                    Button {
+                        //action
+                    } label: {
+                        Image("arrow")
+                            .frame(width: 34, height: 34, alignment: .center)
+                            .background(accentLightColor).clipShape(Circle())
                     }
+
                 }
             }
-        }
+        }.frame(width: 400, height: .infinity, alignment: .leading)
     }
 }
 
 @available(OSX 10.15.0, *)
 struct PieChartView_Previews: PreviewProvider {
     static var previews: some View {
-        PieChartView(values: [1300, 500, 300], names: ["Rent", "Transport", "Education"],
-                     formatter: {value in String(format: "$%.2f", value)},backgroundColor: .white, widthFraction: 0.3, foregroundColor: .black)
-        .frame(width: 800, height: 180, alignment: .leading)
+        PieChartView(values: [1300, 500, 300], names: ["Ordini Temporanei", "Ordini inviati", "Ordini sospesi"],
+                     formatter: {value in String(format: "$%.2f", value)}, backgroundColor: .white,
+                     widthFraction: 0.3, foregroundColor: .black, accentColor: Color.blue, accentLightColor: Color.blue.opacity(0.5))
+        .previewLayout(.fixed(width: 1100, height: 330))
         
     }
 }
